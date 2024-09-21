4ec9eaf23e83463aca22e74e5a9cc990cf75fb5b
ajaykainth <158183918+ajaykainth@users.noreply.github.com>
2024-09-21 14:19:48 +0530

    .

--- Backend/server/config/journeyfyDb.js
+++ Backend/server/config/journeyfyDb.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 mongoose.connect(process.env.MONGO_URL)
-// console.log(process.env.MONGO_URL)
+
 
     .then(() => {
         console.log("DB connection successful");
+        // console.log(process.env.MONGO_URL)
+
     })
     .catch((err) => {
         console.log("Error in DB connection", err);
